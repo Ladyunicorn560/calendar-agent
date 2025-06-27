ef983471f77d4b581e0b3dfdfffb52dba63caedb
Shiksha Shrivastava <132224370+Ladyunicorn560@users.noreply.github.com>
2025-06-28 04:19:09 +0530

    Update calendar_utils.py

--- backend/calendar_utils.py
+++ backend/calendar_utils.py
@@ -4,9 +4,6 @@ from __future__ import print_function
 import datetime
 import os
 import json
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import streamlit as st
 from google.oauth2 import service_account
